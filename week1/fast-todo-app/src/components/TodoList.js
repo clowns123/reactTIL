d083f142730604d669f9cd45bcdd84d3d3e60fcc
Hwang taehyeon <clown1232@gmail.com>
2020-07-26 15:23:01 +0800

    test:

---
+++ week1/fast-todo-app/src/components/TodoList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import TodoItem from "./TodoItem";
+
+const TodoListBlock = styled.div`
+  flex: 1;
+  padding: 20px 32px;
+  padding-bottom: 48px;
+  overflow-y: auto;
+`;
+const TodoList = () => {
+  return (
+    <TodoListBlock>
+      <TodoItem text="test" done={true} />
+      <TodoItem text="asdf" done={true} />
+      <TodoItem text="qwer" done={true} />
+      <TodoItem text="zxcv" done={false} />
+    </TodoListBlock>
+  );
+};
+
+export default TodoList;
